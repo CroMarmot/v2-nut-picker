56781d9a37a2562028b32fa81567d575e953e98b
cromarmot <yexiaorain@gmail.com>
2020-08-21 15:58:14 +0800

    init

---
+++ docs/basic.md
@@ -0,0 +1,15 @@
+基本用法
+
+```vue
+<template>
+  <v2-m-picker/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+```
---
+++ src/v2-m-picker.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="v2-m-picker" v-bind="$attrs" v-on="$listeners">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'V2MPicker',
+  props: {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less"></style>
